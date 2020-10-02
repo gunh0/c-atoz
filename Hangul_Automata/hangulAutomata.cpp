ab24c65b1c5575852e22ff2b9975aa421b6a5d66
Gunho Park <devgun@kakao.com>
2020-10-02 18:54:42 +0900

    Add JMO converter

--- Hangul_Automata/hangulAutomata.cpp
+++ Hangul_Automata/hangulAutomata.cpp
@@ -22,6 +22,13 @@ string input_all[] = {"hangug woigukweo",
                       "hangukwoigukweodaehakgyo keompyuteogowhakbu",
                       "keompawilreoneun jaemiwitneun goamokwida.",
                       "wyulsimhi gowbuhaewyagetda."};
+string temp;
+string state = "0"; // Inital state
+string output;
+
+vector<char> J = {'g', 'n', 'd', 'r', 'm', 'b', 's', 'w', 'j', 'z', 'k', 't', 'p', 'h', 'l', 'q', 'f', 'c', 'v'};
+vector<char> M = {'a', 'e', 'i', 'o', 'u', 'y'};
+vector<char> O = {' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '*', '/', '[', ']', '.', ',', '!', '?'};
 
 vector<string> split(string str, char delimiter)
 {
@@ -37,17 +44,37 @@ vector<string> split(string str, char delimiter)
     return internal;
 }
 
+char JMO_Converter(char ch)
+{
+    for (int j = 0; j < J.size(); j++)
+        if(ch==J[j]) return 'J';
+    for (int m = 0; m < M.size(); m++)
+        if(ch==M[m]) return 'M';
+    // for (int o = 0; o < O.size(); o++)
+    //     if(ch==O[o]) return 'O';
+    return 'O';
+}
+
 int main()
 {
-    char J[] = {'g', 'n', 'd', 'r', 'm', 'b', 's', 'w', 'j', 'z', 'k', 't', 'p', 'h', 'l', 'q', 'f', 'c', 'v'};
-    char M[] = {'a', 'e', 'i', 'o', 'u', 'y'};
-    char O[] = {' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '*', '/', '[', ']', '.', ',', '!', '?'};
 
     for (int i = 0; i < sizeof(input_all) / sizeof(input_all[0]); i++)
     {
-        cout << input_all[i] << endl;
+        //cout << input_all[i] << endl;
         vector<string> input_str = split(input_all[i], ' ');
-        cout << input_str.size() << endl;
+        //cout << input_str.size() << endl;
+
+        for (int j = 0; j < input_str.size(); j++)
+        {
+            for (int k = 0; k < input_str[j].length(); k++)
+            {
+                char ch = input_str[j][k];
+                char conv_ch = JMO_Converter(ch);
+                cout << conv_ch;
+            }
+            cout << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
