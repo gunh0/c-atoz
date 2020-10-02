4eb9eb92d97cfaf750d424e267a69cd6615b83cd
Gunho Park <devgun@kakao.com>
2020-10-02 21:54:52 +0900

    Done.

--- Hangul_Automata/hangulAutomata.cpp
+++ Hangul_Automata/hangulAutomata.cpp
@@ -18,38 +18,25 @@
 
 using namespace std;
 
-string input_all[] = {"hangug woigukweo",
+/////////////////////////// 문장을 입력해주세요. /////////////////////////////
+string input_all[] = {"hangug woigukweo", "parkgunho",
                       "hangukwoigukweodaehakgyo keompyuteogowhakbu",
                       "keompawilreoneun jaemiwitneun goamokwida.",
                       "wyulsimhi gowbuhaewyagetda."};
-string temp;
-string state = "0"; // Inital state
-string output;
+////////////////////////////////////////////////////////////////////////////
 
 vector<char> J = {'g', 'n', 'd', 'r', 'm', 'b', 's', 'w', 'j', 'z', 'k', 't', 'p', 'h', 'l', 'q', 'f', 'c', 'v'};
 vector<char> M = {'a', 'e', 'i', 'o', 'u', 'y'};
 vector<char> O = {' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '*', '/', '[', ']', '.', ',', '!', '?'};
 
-vector<string> split(string str, char delimiter)
-{
-    vector<string> internal;
-    stringstream ss(str);
-    string temp;
-
-    while (getline(ss, temp, delimiter))
-    {
-        internal.push_back(temp);
-    }
-
-    return internal;
-}
-
 char JMO_Converter(char ch)
 {
     for (int j = 0; j < J.size(); j++)
-        if(ch==J[j]) return 'J';
+        if (ch == J[j])
+            return 'J';
     for (int m = 0; m < M.size(); m++)
-        if(ch==M[m]) return 'M';
+        if (ch == M[m])
+            return 'M';
     // for (int o = 0; o < O.size(); o++)
     //     if(ch==O[o]) return 'O';
     return 'O';
@@ -60,21 +47,143 @@ int main()
 
     for (int i = 0; i < sizeof(input_all) / sizeof(input_all[0]); i++)
     {
-        //cout << input_all[i] << endl;
-        vector<string> input_str = split(input_all[i], ' ');
-        //cout << input_str.size() << endl;
+        cout << input_all[i] << endl;
+        string input_str = input_all[i];
+        //cout << input_str.length() << endl;
 
-        for (int j = 0; j < input_str.size(); j++)
+        string temp = "";
+        string state = "0"; // Inital state
+        string output = "";
+
+        for (int j = 0; j < input_str.length(); j++)
         {
-            for (int k = 0; k < input_str[j].length(); k++)
+            char ch = input_str[j];
+            char conv_ch = JMO_Converter(ch);
+            if (state.compare("0") == 0)
             {
-                char ch = input_str[j][k];
-                char conv_ch = JMO_Converter(ch);
-                cout << conv_ch;
+                switch (conv_ch)
+                {
+                case 'O':
+                    output = output + ch + "|";
+                    state = "0";
+                    break;
+                case 'J':
+                    state += "J";
+                    temp += ch;
+                    break;
+                case 'M': // error
+                    state = "0";
+                    temp = "";
+                    break;
+                }
             }
-            cout << " ";
+            else if (state.compare("0J") == 0)
+            {
+                switch (conv_ch)
+                {
+                case 'O': // error
+                    state = "0";
+                    temp = "";
+                    break;
+                case 'J': // error
+                    state = "0";
+                    temp = "";
+                    break;
+                case 'M':
+                    state += "M";
+                    temp += ch;
+                    break;
+                }
+            }
+            else if (state.compare("0JM") == 0)
+            {
+                switch (conv_ch)
+                {
+                case 'O':
+                    output = output + temp + "|" + ch + "|";
+                    state = "0";
+                    temp = "";
+                    break;
+                case 'J':
+                    state += "J";
+                    temp += ch;
+                    break;
+                case 'M':
+                    state += "M";
+                    temp += ch;
+                    break;
+                }
+            }
+            else if (state.compare("0JMM") == 0)
+            {
+                switch (conv_ch)
+                {
+                case 'O':
+                    output = output + temp + "|" + ch + "|";
+                    state = "0";
+                    temp = "";
+                    break;
+                case 'J':
+                    state += "J";
+                    temp += ch;
+                    break;
+                case 'M': // error
+                    output = output + temp + "|";
+                    state = "0";
+                    temp = "";
+                    break;
+                }
+            }
+            else if (state.compare("0JMJ") == 0)
+            {
+                switch (conv_ch)
+                {
+                case 'O':
+                    output = output + temp + "|" + ch + "|";
+                    state = "0";
+                    temp = "";
+                    break;
+                case 'J':
+                    state += "J";
+                    temp += ch;
+                    break;
+                case 'M':
+                    output = output + temp[0] + temp[1] + "|";
+                    state = "0JM";
+                    temp = temp[2];
+                    temp += ch;
+                    break;
+                }
+            }
+            else if ((state.compare("0JMMJ") == 0) || (state.compare("0JMJJ") == 0))
+            {
+                switch (conv_ch)
+                {
+                case 'O':
+                    output = output + temp + "|" + ch + "|";
+                    state = "0";
+                    temp = "";
+                    break;
+                case 'J':
+                    output = output + temp + "|";
+                    state = "0J";
+                    temp = ch;
+                    break;
+                case 'M':
+                    output = output + temp[0] + temp[1] + temp[2] + "|";
+                    state = "0JM";
+                    temp = temp[3];
+                    temp += ch;
+                    break;
+                }
+            }
+            // cout << temp << endl;    // for debuging
         }
+        if (temp != "")
+            output = output + temp + '|';
+        cout << output;
         cout << endl;
     }
+
     return 0;
 }
